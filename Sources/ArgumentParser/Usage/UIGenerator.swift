78f492ac88cdaf12fa1a8b415e11962f650197aa
Anton Leuski <anton@leuski.net>
2020-11-07 18:30:48 -0800

    Updating metadata

--- Sources/ArgumentParser/Usage/UIGenerator.swift
+++ Sources/ArgumentParser/Usage/UIGenerator.swift
@@ -5,32 +5,7 @@
 //  Created by Anton Leuski on 11/4/20.
 //
 
-protocol OptionGroupProtocol {
-  static var _valueType: ParsableArguments.Type { get }
-}
-extension OptionGroup: OptionGroupProtocol {
-  static var _valueType: ParsableArguments.Type { Value.self }
-}
-protocol ArgumentProtocol {
-  static var _valueType: Any.Type { get }
-}
-extension Argument: ArgumentProtocol {
-  static var _valueType: Any.Type { Value.self }
-}
-protocol FlagProtocol {
-  static var _valueType: Any.Type { get }
-}
-extension Flag: FlagProtocol {
-  static var _valueType: Any.Type { Value.self }
-}
-protocol OptionProtocol {
-  static var _valueType: Any.Type { get }
-}
-extension Option: OptionProtocol {
-  static var _valueType: Any.Type { Value.self }
-}
-
-public struct UIMetadata {
+public struct PropertyMetadata: Identifiable {
   public enum Kind {
     case long(String)
     case short(Character)
@@ -38,51 +13,101 @@ public struct UIMetadata {
     case positional
   }
 
-  public enum ArgumentKind {
-    case flag(Any.Type)
-    case argument(Any.Type)
-    case option(Any.Type)
+  public struct Content {
+    public let help: ArgumentHelp?
+    public let kind: Kind
+    public let valueName: String
+
+    fileprivate init(_ arg: ArgumentDefinition) {
+      let kind: PropertyMetadata.Kind
+      switch arg.kind {
+      case .positional: kind = .positional
+      case .named(let names):
+        switch names.first {
+        case .none: fatalError()
+        case .long(let value): kind = .long(value)
+        case .short(let value): kind = .short(value)
+        case .longWithSingleDash(let value): kind = .longWithSingleDash(value)
+        }
+      }
+
+      self.help = arg.help.help
+      self.kind = kind
+      self.valueName = arg.valueName
+    }
   }
 
-  public let help: ArgumentHelp?
-  public let key: String
-  public let defaultValue: String?
-  public let kind: Kind
-  public let argumentKind: ArgumentKind
-  public let valueName: String
+  public let id: String
+  public let initialValue: Any?
+  public let values: [Content]
+  public let type: Any.Type
+
+  init?(_ arg: ArgumentSet, key: InputKey, id: String, type: Any.Type) {
+    self.type = type
+    self.id = id
+    self.values = arg.map { Content($0) }
+    guard !values.isEmpty else { return nil }
+
+    if let arg1 = Array(arg).first {
+      do {
+        var values = ParsedValues(originalInput: [])
+        try arg1.initial(InputOrigin(), &values)
+        self.initialValue = values.elements[key]?.value
+      } catch {
+        self.initialValue = nil
+      }
+    } else {
+      self.initialValue = nil
+    }
+  }
 
   public var abstract: String? {
-    (help?.abstract).flatMap { $0.isEmpty ? nil : $0} }
+    (values.first?.help?.abstract).flatMap { $0.isEmpty ? nil : $0 }  }
   public var discussion: String? {
-    (help?.discussion).flatMap { $0.isEmpty ? nil : $0} }
+    (values.first?.help?.discussion).flatMap { $0.isEmpty ? nil : $0 } }
+
+  var first: Content { values.first ?? {fatalError("cannot happen")}() }
 }
 
-private func _parse(_ type: ParsableArguments.Type)
-  -> [(String, UIMetadata.ArgumentKind)]
+private protocol _OptionGroupProtocol {
+  static var _valueType: ParsableArguments.Type { get }
+}
+extension OptionGroup: _OptionGroupProtocol {
+  static var _valueType: ParsableArguments.Type { Value.self }
+}
+
+private func _parse(_ type: ParsableArguments.Type, key: String = "")
+-> [PropertyMetadata]
 {
   Mirror(reflecting: type.init())
     .children
-    .flatMap { child -> [(String, UIMetadata.ArgumentKind)] in
+    .flatMap { child -> [PropertyMetadata] in
       guard
-        var codingKey = child.label,
-        let parsed = child.value as? ArgumentSetProvider
+        let childLabel = child.label
       else { return [] }
 
       // Property wrappers have underscore-prefixed names
-      codingKey = String(codingKey.dropFirst(codingKey.first == "_" ? 1 : 0))
+      let codingKey = String(childLabel.dropFirst(
+                              childLabel.first == "_" ? 1 : 0))
 
-      switch Swift.type(of: parsed) {
-      case let type as OptionGroupProtocol.Type:
-        return _parse(type._valueType)
+      let nextKey = key + ".\(codingKey)"
 
-      case let type as ArgumentProtocol.Type:
-        return [(codingKey, .argument(type._valueType))]
+      switch Swift.type(of: child.value) {
+      case let type as _OptionGroupProtocol.Type:
+        return _parse(type._valueType, key: nextKey)
 
-      case let type as FlagProtocol.Type:
-        return [(codingKey, .flag(type._valueType))]
+      case is ArgumentSetProvider.Type:
 
-      case let type as OptionProtocol.Type:
-        return [(codingKey, .option(type._valueType))]
+        guard
+          let parsed = child.value as? ArgumentSetProvider
+        else { return [] }
+
+        let key = InputKey(rawValue: codingKey)
+        let argumentSet = parsed.argumentSet(for: key)
+        let metadata = PropertyMetadata(
+          argumentSet, key: key, id: nextKey,
+          type: Swift.type(of: child.value))
+        return metadata.map { [$0] } ?? []
 
       default:
         return []
@@ -90,54 +115,6 @@ private func _parse(_ type: ParsableArguments.Type)
     }
 }
 
-struct UIGenerator {
-
-  let metadata: [UIMetadata]
-
-  init(commandStack: [ParsableCommand.Type]) {
-    guard let command = commandStack.last else { fatalError() }
-
-    let types = Dictionary(_parse(command), uniquingKeysWith: {_, new in new })
-
-    metadata = commandStack.flatMap {
-      Array(ArgumentSet($0)).compactMap {
-        arg in
-        guard arg.help.help?.shouldDisplay != false else { return nil }
-
-        let kind: UIMetadata.Kind
-        switch arg.kind {
-        case .positional: kind = .positional
-        case .named(let names):
-          switch names.first {
-          case .none: fatalError()
-          case .long(let value): kind = .long(value)
-          case .short(let value): kind = .short(value)
-          case .longWithSingleDash(let value): kind = .longWithSingleDash(value)
-          }
-        }
-
-        guard let key = arg.help.keys.map({ $0.rawValue })
-                .first(where: {nil != types[$0]}),
-              let argumentKind = types[key]
-        else { return nil }
-
-        return UIMetadata(
-          help: arg.help.help,
-          key: key,
-          defaultValue: arg.help.defaultValue,
-          kind: kind,
-          argumentKind: argumentKind,
-          valueName: arg.valueName)
-      }
-    }
-  }
-}
-
 extension ParsableCommand {
-  public static func ui(for subcommand: ParsableCommand.Type)
-  -> [UIMetadata]
-  {
-    let stack = CommandParser(self).commandStack(for: subcommand)
-    return UIGenerator(commandStack: stack).metadata
-  }
+  public static var metadata: [PropertyMetadata] { _parse(self) }
 }
