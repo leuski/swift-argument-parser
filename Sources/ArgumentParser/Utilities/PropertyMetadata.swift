dcfa9d38106c63c0dc81438a34394a153bd81398
Anton Leuski <anton@leuski.net>
2024-06-09 20:59:01 -0600

    adopting the changes from 1.4 upstream version

--- Sources/ArgumentParser/Utilities/PropertyMetadata.swift
+++ Sources/ArgumentParser/Utilities/PropertyMetadata.swift
@@ -21,7 +21,7 @@ fileprivate extension Collection {
 // - and the type of the property wrapper in case we need to warn the
 //    programmer about a missing editor
 
-public struct PropertyMetadata: Identifiable {
+public struct PropertyMetadata {
   // Copying from ArgumentDefinition
   /// This folds the public `ArrayParsingStrategy` and
   /// `SingleValueParsingStrategy`
@@ -42,16 +42,8 @@ public struct PropertyMetadata: Identifiable {
     /// Parse all remaining `SplitArguments.Element`
     /// as values, regardless of its type.
     case allRemainingInput
-
-    init(_ value: ArgumentDefinition.ParsingStrategy) {
-      switch value {
-      case .default: self = .default
-      case .scanningForValue: self = .scanningForValue
-      case .unconditional: self = .unconditional
-      case .upToNextOption: self = .upToNextOption
-      case .allRemainingInput: self = .allRemainingInput
-      }
-    }
+    case postTerminator
+    case allUnrecognized
   }
 
   /// Information about an argument's name.
@@ -89,15 +81,15 @@ public struct PropertyMetadata: Identifiable {
 
   public let kind: Kind
   /// All names of the argument.
-  public var names: [NameInfo]?
+  public let names: [NameInfo]?
   /// The best name to use when referring to the argument in help displays.
-  public var preferredName: NameInfo?
+  public let preferredName: NameInfo?
   /// Name of argument's value.
   public let valueName: String?
   /// Short description of the argument's functionality.
-  public var abstract: String?
+  public let abstract: String?
   /// Extended description of the argument's functionality.
-  public var discussion: String?
+  public let discussion: String?
 
   public let parsingStrategy: ParsingStrategy
 
@@ -106,17 +98,18 @@ public struct PropertyMetadata: Identifiable {
   public let initialValue: Any?
 
   public let type: Any.Type
+}
 
-  init?(
-    _ arg: ArgumentSet, key: InputKey, id: String, type: Any.Type)
-  {
-    self.type = type
-    self.id = id
+fileprivate extension PropertyMetadata {
+  init?(_ arg: ArgumentSet, key: InputKey, type: Any.Type) {
     guard
       let argument = arg.first(where: { Kind(argument: $0) != nil }),
       let kind = Kind(argument: argument)
     else { return nil }
 
+    self.type = type
+    // the initial period is here for historical reasons.
+    self.id = "." + key.fullPath.joined(separator: ".")
     self.kind = kind
     self.names = argument.names.map(NameInfo.init)
     self.preferredName = argument.names.preferredName.map(NameInfo.init)
@@ -131,6 +124,20 @@ public struct PropertyMetadata: Identifiable {
       self.initialValue = values.elements[key]?.value
     } catch {
       self.initialValue = nil
+    }
+  }
+}
+
+fileprivate extension PropertyMetadata.ParsingStrategy {
+  init(_ value: ArgumentDefinition.ParsingStrategy) {
+    switch value {
+    case .default: self = .default
+    case .scanningForValue: self = .scanningForValue
+    case .unconditional: self = .unconditional
+    case .upToNextOption: self = .upToNextOption
+    case .allRemainingInput: self = .allRemainingInput
+    case .postTerminator: self = .postTerminator
+    case .allUnrecognized: self = .allUnrecognized
     }
   }
 }
@@ -166,51 +173,41 @@ fileprivate extension PropertyMetadata.NameInfo {
   }
 }
 
-private protocol _OptionGroupProtocol {
-  static var _valueType: ParsableArguments.Type { get }
-}
-extension OptionGroup: _OptionGroupProtocol {
-  static var _valueType: ParsableArguments.Type { Value.self }
+private protocol _MetadataExtractor {
+  func _metadata(for key: InputKey) -> [PropertyMetadata]?
 }
 
-private func _parse(_ type: ParsableArguments.Type, key: String = "")
+extension _MetadataExtractor where Self: ParsedWrapper {
+  func _metadata(for key: InputKey) -> [PropertyMetadata]? {
+    let argumentSet = argumentSet(for: key)
+    let metadata = PropertyMetadata(
+      argumentSet, key: key, type: Swift.type(of: self))
+    return metadata.map { [$0] }
+  }
+}
+
+extension OptionGroup: _MetadataExtractor {
+  func _metadata(for key: InputKey) -> [PropertyMetadata]? {
+    _parse(Value.self, parent: key)
+  }
+}
+
+extension Argument: _MetadataExtractor {}
+extension Option: _MetadataExtractor {}
+extension Flag: _MetadataExtractor {}
+
+private func _parse(_ type: ParsableArguments.Type, parent: InputKey? = nil)
 -> [PropertyMetadata]
 {
   Mirror(reflecting: type.init())
     .children
-    .flatMap { child -> [PropertyMetadata] in
-      guard
-        let childLabel = child.label
-      else { return [] }
-
-      // Property wrappers have underscore-prefixed names
-      let codingKey = String(childLabel.dropFirst(
-        childLabel.first == "_" ? 1 : 0))
-
-      let nextKey = key + ".\(codingKey)"
-      let childType = Swift.type(of: child.value)
-
-      switch childType {
-      case let optionGroupType as _OptionGroupProtocol.Type:
-        return _parse(optionGroupType._valueType, key: nextKey)
-
-      case is ArgumentSetProvider.Type:
-
-        guard
-          let parsed = child.value as? ArgumentSetProvider
-        else { return [] }
-
-        let key = InputKey(rawValue: codingKey)
-        let argumentSet = parsed.argumentSet(for: key)
-        let metadata = PropertyMetadata(
-          argumentSet, key: key, id: nextKey,
-          type: childType)
-        return metadata.map { [$0] } ?? []
-
-      default:
-        return []
-      }
+    .compactMap { child -> [PropertyMetadata]? in
+      guard let childLabel = child.label
+      else { return nil }
+      let key = InputKey(name: childLabel, parent: parent)
+      return (child.value as? _MetadataExtractor)?._metadata(for: key)
     }
+    .flatMap { $0 }
 }
 
 public struct CommandInfo {
